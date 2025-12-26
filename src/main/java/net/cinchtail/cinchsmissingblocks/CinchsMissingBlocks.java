3ee6669c2d27455f616b53ce40fa7ab233ea01b2
Joshua Davies <cinchtail@gmail.com>
2025-12-26 18:13:49 -0500

    Added project files

---
+++ src/main/java/net/cinchtail/cinchsmissingblocks/CinchsMissingBlocks.java
@@ -0,0 +1,23 @@
+package net.cinchtail.cinchsmissingblocks;
+
+import net.cinchtail.cinchsmissingblocks.block.ModBlocks;
+import net.cinchtail.cinchsmissingblocks.item.ModCreativeModeTabs;
+import net.cinchtail.cinchsmissingblocks.item.ModItems;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.LoggerFactory;
+
+@Mod(CinchsMissingBlocks.MOD_ID)
+public class CinchsMissingBlocks {
+    public static final String MOD_ID = "cinchsmissingblocks";
+    public static final Logger LOGGER = LoggerFactory.getLogger(CinchsMissingBlocks.class);
+
+    public CinchsMissingBlocks(@NotNull IEventBus bus) {
+        ModItems.ITEMS.register(bus);
+        ModBlocks.BLOCKS.register(bus);
+        ModCreativeModeTabs.CREATIVE_MODE_TABS.register(bus);
+    }
+}
