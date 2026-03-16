44e98ae6dbc01b095000b21894472847cf765832
Joshua Davies <cinchtail@gmail.com>
2026-03-15 21:51:04 -0400

    fixed resourcepacks and working on mixin

--- src/main/java/net/cinchtail/cinchsmissingblocks/mixins/ChiseledTuffBricksMixin.java
+++ src/main/java/net/cinchtail/cinchsmissingblocks/mixins/ChiseledTuffBricksMixin.java
@@ -16,5 +16,11 @@ import org.spongepowered.asm.mixin.injection.At;
 @Mixin(Blocks.class)
 public abstract class ChiseledTuffBricksMixin {
 
-
+    @Definition(id = "register", method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Lnet/minecraft/block/Block;)Lnet/minecraft/block/Block;")
+    @Definition(id = "Block", type = Block.class)
+    @Expression("register('chiseled_tuff_bricks', @(new Block(?)))")
+    @WrapOperation(method = "<clinit>", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private static Block makeChiseledTuffBricksPillarBlock(AbstractBlock.Settings settings, Operation<Block> original) {
+        return PillarBlock::new, AbstractBlock.Settings.create();
+    }
 }
