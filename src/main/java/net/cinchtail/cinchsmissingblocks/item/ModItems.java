21e5db8b986d795a99f90926ef11baca823f7355
Joshua Davies <cinchtail@gmail.com>
2026-04-03 13:19:31 -0400

    Added GitIgnore

--- src/main/java/net/cinchtail/cinchsmissingblocks/item/ModItems.java
+++
@@ -1,8 +0,0 @@
-package net.cinchtail.cinchsmissingblocks.item;
-
-import net.cinchtail.cinchsmissingblocks.CinchsMissingBlocks;
-import net.neoforged.neoforge.registries.DeferredRegister;
-
-public class ModItems {
-    public static DeferredRegister.Items ITEMS = DeferredRegister.createItems(CinchsMissingBlocks.MOD_ID);
-}
