87ce89da68f04ed39892754a19115bc46a81a9cc
Joshua Davies <cinchtail@gmail.com>
2026-04-26 00:52:32 -0400

    working on fixing mixin

--- src/main/java/net/cinchtail/cinchsmissingblocks/mixins/ChiseledTuffBricksMixin.java
+++ src/main/java/net/cinchtail/cinchsmissingblocks/mixins/ChiseledTuffBricksMixin.java
@@ -10,17 +10,24 @@ import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.function.Function;
 
 @Debug(export = true)
 @Mixin(Blocks.class)
 public abstract class ChiseledTuffBricksMixin {
 
-    @Definition(id = "register", method = "Lnet/minecraft/world/level/block/Blocks;register(Ljava/lang/String;Lnet/minecraft/world/level/block/Block;)Lnet/minecraft/world/level/block/Block;")
-    @Definition(id = "Block", type = Block.class)
-    @Expression("register('chiseled_tuff_bricks', @(new Block(?)))")
+    @Shadow
+    private static Block register(String id, Function<BlockBehaviour.Properties, Block> factory, BlockBehaviour.Properties properties) {
+        throw new IllegalStateException();
+    }
+
+    @Definition(id = "register", method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;")
+    @Expression("register('chiseled_tuff_bricks', ?)")
     @WrapOperation(method = "<clinit>", at = @At("MIXINEXTRAS:EXPRESSION"))
-    private static Block makeChiseledTuffBricksPillarBlock(BlockBehaviour.Properties properties, Operation<Block> original) {
-        return new RotatedPillarBlock(properties);
+    private static Block makeChiseledTuffBricksPillarBlock(String id, BlockBehaviour.Properties properties, Operation<Block> original) {
+        return register(id, RotatedPillarBlock::new, properties);
     }
 }
