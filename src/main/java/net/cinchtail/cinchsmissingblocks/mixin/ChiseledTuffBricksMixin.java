1312e52a2034eccf07b6cfcdbc37a6b7abfc7c5f
Joshua Davies <cinchtail@gmail.com>
2026-05-19 00:16:01 -0400

    configs, json file stuff + more

---
+++ src/main/java/net/cinchtail/cinchsmissingblocks/mixin/ChiseledTuffBricksMixin.java
@@ -0,0 +1,26 @@
+package net.cinchtail.cinchsmissingblocks.mixin;
+
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Debug(export = true)
+@Mixin(Blocks.class)
+public abstract class ChiseledTuffBricksMixin {
+
+    @Definition(id = "register", method = "Lnet/minecraft/world/level/block/Blocks;register(Ljava/lang/String;Lnet/minecraft/world/level/block/Block;)Lnet/minecraft/world/level/block/Block;")
+    @Definition(id = "Block", type = Block.class)
+    @Expression("register('chiseled_tuff_bricks', @(new Block(?)))")
+    @WrapOperation(method = "<clinit>", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private static Block makeChiseledTuffBricksPillarBlock(BlockBehaviour.Properties properties, Operation<Block> original) {
+        return new RotatedPillarBlock(properties);
+    }
+}
---
+++ src/main/java/net/cinchtail/cinchsmissingblocks/pack/ModBuiltinPacks.java
@@ -0,0 +1,56 @@
+package net.cinchtail.cinchsmissingblocks.pack;
+
+import net.cinchtail.cinchsmissingblocks.config.ModConfigs;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.repository.Pack;
+import net.minecraft.server.packs.repository.PackSource;
+import net.neoforged.neoforge.event.AddPackFindersEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModBuiltinPacks {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("cinchsmissingblocks");
+
+    public static void register(AddPackFindersEvent event, String modId) {
+
+        boolean useBuiltInSource = ModConfigs.doubleSlabsPackDefaultEnabled;
+
+        PackSource source = useBuiltInSource
+                ? PackSource.DEFAULT
+                : PackSource.BUILT_IN;
+
+        event.addPackFinders(
+                ResourceLocation.fromNamespaceAndPath(modId, "resource_packs/cinchs_double_slabs"),
+                PackType.CLIENT_RESOURCES,
+                Component.literal("Cinch's Double Slabs"),
+                source,
+                false,
+                Pack.Position.TOP
+        );
+
+        if (ModConfigs.enableTuffBrickPillar) {
+            event.addPackFinders(
+                    ResourceLocation.fromNamespaceAndPath(modId, "resource_packs/cinchs_tuff_pillars"),
+                    PackType.CLIENT_RESOURCES,
+                    Component.literal("Cinch's Tuff Pillars"),
+                    PackSource.BUILT_IN,
+                    true,
+                    Pack.Position.TOP
+            );
+
+            event.addPackFinders(
+                    ResourceLocation.fromNamespaceAndPath(modId, "resource_packs/cinchs_tuff_pillar_recipes"),
+                    PackType.SERVER_DATA,
+                    Component.literal("Cinch's Tuff Pillars Recipes"),
+                    PackSource.BUILT_IN,
+                    false,
+                    Pack.Position.TOP
+            );
+        } else {
+            LOGGER.info("Cinch's Tuff Pillars datapack disabled by config");
+        }
+    }
+}
