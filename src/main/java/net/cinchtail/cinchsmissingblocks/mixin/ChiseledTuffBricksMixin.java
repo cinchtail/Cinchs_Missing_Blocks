fa19eedf025534d5413a251a444581ccebe5bb54
Joshua Davies <cinchtail@gmail.com>
2026-07-16 18:31:43 -0400

    working on mixin

--- src/main/java/net/cinchtail/cinchsmissingblocks/mixin/ChiseledTuffBricksMixin.java
+++ src/main/java/net/cinchtail/cinchsmissingblocks/mixin/ChiseledTuffBricksMixin.java
@@ -2,10 +2,8 @@ package net.cinchtail.cinchsmissingblocks.mixin;
 
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.references.BlockItemId;
-import net.minecraft.references.BlockItemIds;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RotatedPillarBlock;
@@ -21,15 +19,17 @@ import java.util.function.Function;
 @Mixin(Blocks.class)
 public abstract class ChiseledTuffBricksMixin {
 
-    /*@Shadow
-    private static Block register(BlockItemId id, Function<BlockBehaviour.Properties, Block> factory, BlockBehaviour.Properties properties) {
+    @Shadow
+    private static Block register(final BlockItemId id, final Function<BlockBehaviour.Properties, Block> factory, final BlockBehaviour.Properties properties) {
         throw new IllegalStateException();
     }
 
     @Definition(id = "register", method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;")
-    @Expression("register('chiseled_tuff_bricks', ?)")
+    @Definition(id = "BlockItemIds", method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;")
+    @Definition(id = "CHISELED_TUFF_BRICKS", field = "Lnet/minecraft/world/level/block/Blocks;CHISELED_TUFF_BRICKS")
+    @Expression("register(BlockItemIds.CHISELED_TUFF_BRICKS, ?)")
     @WrapOperation(method = "<clinit>", at = @At("MIXINEXTRAS:EXPRESSION"))
-    private static Block makeChiseledTuffBricksPillarBlock(BlockItemId id, BlockBehaviour.Properties properties, Operation<Block> original) {
+    private static Block makeChiseledTuffBricksPillarBlock(BlockItemId id, Function<BlockBehaviour.Properties, Block> factory, BlockBehaviour.Properties properties) {
         return register(id, RotatedPillarBlock::new, properties);
-    }*/
+    }
 }
