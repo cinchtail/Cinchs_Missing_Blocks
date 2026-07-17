dcc61f8dd30ca053bfb79fba29eb73fb936e305f
Joshua Davies <cinchtail@gmail.com>
2026-07-17 18:59:17 -0400

    mixin working

--- src/main/java/net/cinchtail/cinchsmissingblocks/mixin/ChiseledTuffBricksMixin.java
+++ src/main/java/net/cinchtail/cinchsmissingblocks/mixin/ChiseledTuffBricksMixin.java
@@ -2,6 +2,7 @@ package net.cinchtail.cinchsmissingblocks.mixin;
 
 import com.llamalad7.mixinextras.expression.Definition;
 import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.references.BlockItemId;
 import net.minecraft.world.level.block.Block;
@@ -20,16 +21,15 @@ import java.util.function.Function;
 public abstract class ChiseledTuffBricksMixin {
 
     @Shadow
-    private static Block register(final BlockItemId id, final Function<BlockBehaviour.Properties, Block> factory, final BlockBehaviour.Properties properties) {
+    private static Block register(BlockItemId id, Function<BlockBehaviour.Properties, Block> factory, BlockBehaviour.Properties properties) {
         throw new IllegalStateException();
     }
 
-    @Definition(id = "register", method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;")
-    @Definition(id = "BlockItemIds", method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;")
-    @Definition(id = "CHISELED_TUFF_BRICKS", field = "Lnet/minecraft/world/level/block/Blocks;CHISELED_TUFF_BRICKS")
-    @Expression("register(BlockItemIds.CHISELED_TUFF_BRICKS, ?)")
+    @Definition(id = "register", method = "Lnet/minecraft/world/level/block/Blocks;register(Lnet/minecraft/references/BlockItemId;Lnet/minecraft/world/level/block/state/BlockBehaviour$Properties;)Lnet/minecraft/world/level/block/Block;")
+    @Definition(id = "CHISELED_TUFF_BRICKS", field = "Lnet/minecraft/references/BlockItemIds;CHISELED_TUFF_BRICKS:Lnet/minecraft/references/BlockItemId;")
+    @Expression("register(CHISELED_TUFF_BRICKS, ?)")
     @WrapOperation(method = "<clinit>", at = @At("MIXINEXTRAS:EXPRESSION"))
-    private static Block makeChiseledTuffBricksPillarBlock(BlockItemId id, Function<BlockBehaviour.Properties, Block> factory, BlockBehaviour.Properties properties) {
+    private static Block makeChiseledTuffBricksPillarBlock(BlockItemId id, BlockBehaviour.Properties properties, Operation<Block> original) {
         return register(id, RotatedPillarBlock::new, properties);
     }
 }
